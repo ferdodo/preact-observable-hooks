b4fad4bb88b866ec7ca4e418302b4a82e9398c90
ferdodo <ferdodo@hotmail.fr>
2024-06-26 07:15:45 +0000

    Deploying to gh-pages from @ ferdodo/preact-observable-hooks@7f0b89cbaa930ad94f5e7547d185f4db1ab8d081 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIqLU71TypOLSpLTMpJDS5JLElV0lEqSCzJULJSSivNSy7JzM8r1sdUpZdRkpujpKOUnZmXomRlZlIbCwB3pH9fVAAAAA=="
